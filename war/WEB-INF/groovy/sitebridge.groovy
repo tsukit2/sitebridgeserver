0146ce82cf68e7aac4d59a13fe98baf7c04e215f
tsukit <tsukit@c3b38d4a-0ced-01b2-3e13-2d7949194324>
2010-12-19 18:38:21 +0000

    make it works first time on google app

--- war/WEB-INF/groovy/sitebridge.groovy
+++ war/WEB-INF/groovy/sitebridge.groovy
@@ -1,9 +1,8 @@
 // create request object
 def myrequest = [
    method:request.method,
-   path:request.pathInfo,
-   contextPath:request.contextPath,
-   queryStr:request.queryString,
+   pathInfo:params.pathInfo,
+   query:constructQueryMap(request.queryString),
    headers:new HashMap(headers),
    params:new HashMap(params),
    bodyBytes:request.inputStream.bytes
@@ -12,7 +11,27 @@ def myrequest = [
 // then ask request manager to process request
 // this call will block. 
 def manager = new RequestManager(memcache)
-def response = manager.processRequest(myrequest)
+def myresponse = manager.processRequest(myrequest)
 
-request.response = response
-forward '/view/sitebridge/index.gtpl'
+// now relay the information to the original request
+response.status = myresponse.responseDetails.status
+myresponse.responseDetails.headers?.each  { k,v ->
+   response.setHeader(k,v)
+}
+//log.info(new String(myresponse.responseDetails.bodyBytes as byte[]))
+response.outputStream << (myresponse.responseDetails.bodyBytes as byte[])
+
+def constructQueryMap(queryStr) {
+   if (queryStr) {
+      return queryStr.split('&').inject([:]) { m,v -> 
+         def s = v.split('=')
+         if (s[0] != 'pathInfo') {
+            m[s[0]] = s.size() == 2 ? s[1] : ''
+         }
+         return m
+      }
+   } else {
+      return null
+   }
+}
+
--- war/WEB-INF/routes.groovy
+++ war/WEB-INF/routes.groovy
@@ -5,15 +5,18 @@ get "/something", redirect: "/blog/2008/10/20/something", cache: 2.hours
 get "/book/isbn/@isbn", forward: "/WEB-INF/groovy/book.groovy?isbn=@isbn", validate: { isbn ==~ /\d{9}(\d|X)/ }
 */
 
-all '/bridgeconsole',         forward:'/bridgeconsole.groovy?action=index'
-all '/bridgeconsole/@action', forward:'/bridgeconsole.groovy?action=@action'
-all '/**',                    forward:'/sitebridge.groovy/aaa/bbb'
-//all '/@pa/@pb/@pc/@pd/@pe',   forward:'/sitebridge.groovy/@pa/@pb/@pc/@pd/@pe'
-//all '/@pa/@pb/@pc/@pd',       forward:'/sitebridge.groovy/@pa/@pb/@pc/@pd'
-//all '/@pa/@pb/@pc',           forward:'/sitebridge.groovy/@pa/@pb/@pc'
-//all '/@pa/@pb',               forward:'/sitebridge.groovy/@pa/@pb'
-//all '/@pa',                   forward:'/sitebridge.groovy/@pa'
-//all '/',                      forward:'/sitebridge.groovy'
+all '/bridgeconsole',                     forward:'/bridgeconsole.groovy?action=index'
+all '/bridgeconsole/@action',             forward:'/bridgeconsole.groovy?action=@action'
+//all '/**',                                forward:'/sitebridge.groovy'
+all '/@pa/@pb/@pc/@pd/@pe/@pf/@pg/@ph',   forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd/@pe/@pf/@pg/@ph'
+all '/@pa/@pb/@pc/@pd/@pe/@pf/@pg',       forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd/@pe/@pf/@pg'
+all '/@pa/@pb/@pc/@pd/@pe/@pf',           forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd/@pe/@pf'
+all '/@pa/@pb/@pc/@pd/@pe',               forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd/@pe'
+all '/@pa/@pb/@pc/@pd',                   forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd'
+all '/@pa/@pb/@pc',                       forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc'
+all '/@pa/@pb',                           forward:'/sitebridge.groovy?pathInfo=/@pa/@pb'
+all '/@pa',                               forward:'/sitebridge.groovy?pathInfo=/@pa'
+all '/',                                  forward:'/sitebridge.groovy'
 
 // routes for the blobstore service example
 /*

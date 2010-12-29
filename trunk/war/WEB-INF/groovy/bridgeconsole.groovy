366afa2ec07555cab05dc710f020605ba48ccd21
tsukit <tsukit@c3b38d4a-0ced-01b2-3e13-2d7949194324>
2010-12-29 21:15:33 +0000

    try optimize performance

--- trunk/war/WEB-INF/groovy/bridgeconsole.groovy
+++ trunk/war/WEB-INF/groovy/bridgeconsole.groovy
@@ -20,33 +20,47 @@ status = {
    println "system is up"
 }
 
-query = {
-   def manager = new RequestManager(memcache)
-   def requests = []
-   def pendingRequest = null
-   while (pendingRequest = manager.getNextPendingRequest()) {
-      requests << pendingRequest
-   }
+def timeit(label, closure) {
+   def startTime = System.currentTimeMillis()
+   def ret = closure()
+   log.info("${label}: ${System.currentTimeMillis() - startTime} ms")
+   return ret
+}
+
+warmup = {
+   def data = (1..100).collect { new Date() }
+   def sysprop = MiscUtility.convertToMapAndArray(JSONArray.fromObject(data))
+   def deflate = MiscUtility.deflateObjectToByteArray(sysprop)
+   def inflate = MiscUtility.inflateByteArrayToObj(deflate)
 
    headers.contentType = 'text/json'
-   println JSONObject.fromObject(
-      [payload:MiscUtility.deflateObjectToByteArray(
-         JSONArray.fromObject(requests).toString())]).toString()
+   println JSONArray.fromObject(inflate).toString()
+}
+
+query = {
+   def manager = new RequestManager(memcache)
+   def requests = timeit("getNextPendingRequests") { manager.getNextPendingRequests() }
+
+   headers.contentType = 'text/json'
+
+   timeit("Serialize JSON") {
+      println JSONObject.fromObject(
+         [payload:MiscUtility.deflateObjectToByteArray(
+            JSONArray.fromObject(requests).toString())]).toString()
+   }
 }
 
 satisfy = {
    def body = request.reader.text
    //log.info("satisfying ${body}")
-   def responses = JSONArray.fromObject(
+   def responses = timeit("Deserialize input") { JSONArray.fromObject(
       MiscUtility.inflateByteArrayToObj(
          MiscUtility.convertToMapAndArray(JSONObject.fromObject(body).payload) as byte[]))
-
-   // for each response, satisfy it
-   def manager = new RequestManager(memcache)
-   responses.each { 
-      log.info("satisfying request = ${it.responseIndex}")
-      manager.satisfyRequest(it)
    }
+
+   // satisfy all requests
+   def manager = new RequestManager(memcache)
+   timeit("Satisfy request") { manager.satisfyRequests(responses) }
 
    headers.contentType = 'text/json'
    println JSONObject.fromObject([satisfied:true]).toString()

9a290f86e27fc650ad5cd508328ddac478d30667
tsukit <tsukit@c3b38d4a-0ced-01b2-3e13-2d7949194324>
2011-01-14 07:00:40 +0000

    rewrite routes to make it programmatic

--- trunk/war/WEB-INF/routes.groovy
+++ trunk/war/WEB-INF/routes.groovy
@@ -1,26 +1,11 @@
-// example routes
-/*
-get "/blog/@year/@month/@day/@title", forward: "/WEB-INF/groovy/blog.groovy?year=@year&month=@month&day=@day&title=@title"
-get "/something", redirect: "/blog/2008/10/20/something", cache: 2.hours
-get "/book/isbn/@isbn", forward: "/WEB-INF/groovy/book.groovy?isbn=@isbn", validate: { isbn ==~ /\d{9}(\d|X)/ }
-*/
-
+// feel f
 all '/bridgeconsole',                     forward:'/bridgeconsole.groovy?action=index'
 all '/bridgeconsole/@action',             forward:'/bridgeconsole.groovy?action=@action'
-//all '/**',                                forward:'/sitebridge.groovy'
-all '/@pa/@pb/@pc/@pd/@pe/@pf/@pg/@ph',   forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd/@pe/@pf/@pg/@ph'
-all '/@pa/@pb/@pc/@pd/@pe/@pf/@pg',       forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd/@pe/@pf/@pg'
-all '/@pa/@pb/@pc/@pd/@pe/@pf',           forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd/@pe/@pf'
-all '/@pa/@pb/@pc/@pd/@pe',               forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd/@pe'
-all '/@pa/@pb/@pc/@pd',                   forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc/@pd'
-all '/@pa/@pb/@pc',                       forward:'/sitebridge.groovy?pathInfo=/@pa/@pb/@pc'
-all '/@pa/@pb',                           forward:'/sitebridge.groovy?pathInfo=/@pa/@pb'
-all '/@pa',                               forward:'/sitebridge.groovy?pathInfo=/@pa'
-all '/',                                  forward:'/sitebridge.groovy'
 
-// routes for the blobstore service example
-/*
-get "/upload",  forward: "/upload.gtpl"
-get "/success", forward: "/success.gtpl"
-get "/failure", forward: "/failure.gtpl"
-*/
+// do not change these lines unless you know what you are doing. These lines set up path
+// forwarding
+all '/',                                  forward:'/sitebridge.groovy'
+('a'..'z').each { p ->
+   def path = "/@p${('a'..'a').join('/@p')}"
+   all path,                              forward:"/sitebridge.groovy?pathInfo=${path}".toString()
+}
